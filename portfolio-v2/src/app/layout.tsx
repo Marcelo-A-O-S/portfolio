07858461696c337dce780d588fe4ee6563086eec
Marcelo-A-O-S <marceloaugustooliveirasoares@gmail.com>
2024-08-18 20:27:37 -0300

    Update Layout

--- portfolio-v2/src/app/layout.tsx
+++ portfolio-v2/src/app/layout.tsx
@@ -5,7 +5,7 @@ import Container from "./components/Container";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from '@vercel/analytics/react'
 export const metadata: Metadata = {
-  title: "Marcelo Augusto.Dev  || Full Stack",
+  title: "Marcelo Augusto.Dev || Full Stack",
   description: "Olá, meu nome é Marcelo Augusto e sou estudante de programação com foco em desenvolvimento full stack nas horas vagas!",
 };
 
